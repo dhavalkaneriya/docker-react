8c47f2f0632940f198fe696c8842a4218c98b5ef
dhaval <dhavalkaneriya8@gmail.com>
2018-12-23 13:30:13 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>fully functional with docker, travis and aws EBS..</p>
           <a
             className="App-link"
             href="https://reactjs.org"
